e6734997d510249eff6a49b028c3fc3467de2172
cipriansuiu <suiuciprianconstantin@gmail.com>
2017-10-17 15:43:19 +0100

    simple server receiving one message from one client

---
+++ cmake-build-debug/CMakeFiles/untitled8.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled8.dir/main.c.o"
+  "untitled8.pdb"
+  "untitled8.exe"
+  "libuntitled8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
